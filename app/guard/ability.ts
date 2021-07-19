81e45e8f2464cfe0cdfc8de2cccf5795693df819
mrmadhat <danny@danielgregory.dev>
2021-07-19 03:46:44 +0100

    refactor manage article

--- app/guard/ability.ts
+++ app/guard/ability.ts
@@ -8,41 +8,14 @@ type ExtendedAbilityTypes = "send email"
 const Guard = GuardBuilder<ExtendedResourceTypes, ExtendedAbilityTypes>(
   async (ctx, { can, cannot }) => {
     cannot("manage", "all")
-    /*
-		Your rules go here, you can start by removing access to everything
-		and gradually adding the necessary permissions
 
-		eg:
-		cannot("manage", "comment")
-		cannot("manage", "article")
-
-		can("read", "article")
-		can("read", "comment")
-
-    */
     if (ctx.session.$isAuthorized()) {
       can("create", "article")
 
-      can("read", "article", async (_args) => {
+      can("manage", "article", async (args) => {
         return (
           (await db.article.count({
-            where: { id: _args.id, organizationId: ctx.session.orgId },
-          })) === 1
-        )
-      })
-
-      can("update", "article", async (_args) => {
-        return (
-          (await db.article.count({
-            where: { id: _args.id, organizationId: ctx.session.orgId },
-          })) === 1
-        )
-      })
-
-      can("delete", "article", async (_args) => {
-        return (
-          (await db.article.count({
-            where: { id: _args.id, organizationId: ctx.session.orgId },
+            where: { id: args.id, organizationId: ctx.session.orgId },
           })) === 1
         )
       })
